cf1d1572b1b79664ab5bf07ceb529fc941bc1b0b
FedorByssov <bysov1987@mail.ru>
2023-10-09 18:28:24 +0300

    create application.properties

---
+++ java-spring-ru/first-start/src/main/java/exercise/Application.java
@@ -0,0 +1,27 @@
+package exercise;
+
+// BEGIN
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/about")
+    String about() {
+        return "Welcome to Hexlet!";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
+// END
+
