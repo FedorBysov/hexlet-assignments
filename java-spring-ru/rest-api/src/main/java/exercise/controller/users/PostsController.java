7e084a6f158024781e9d515196c4cd12bca45682
FedorByssov <bysov1987@mail.ru>
2023-10-12 16:07:20 +0300

    HomeWork

---
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -0,0 +1,49 @@
+package exercise.controller.users;
+import exercise.Data;
+import exercise.model.Post;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/*
+import org.springframework.http.HttpStatus;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import exercise.model.Post;
+import exercise.Data;
+
+ */
+
+// BEGIN
+
+@RestController
+@RequestMapping("/api")
+class PostsController{
+
+    List<Post> data = Data.getPosts();
+
+    @GetMapping("/users/{id}/posts")
+    public List<Post> show(@PathVariable int id){
+        var stream = data.stream().filter(a ->a.getUserId() == id).toList();
+        return stream;
+    }
+
+    @GetMapping("/posts/{id}/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post create(@PathVariable int id, @RequestBody Post posts){
+        posts.setUserId(id);
+        data.add(posts);
+        return posts;
+    }
+
+}
+
+// END
---
+++ java-spring-ru/rest-api/src/main/java/exercise/model/Post.java
@@ -0,0 +1,78 @@
+package exercise.model;
+
+public class Post {
+    private int userId;
+    private String slug;
+    private String title;
+    private String body;
+
+    public Post() {
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public String getSlug() {
+        return this.slug;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Post)) return false;
+        final Post other = (Post) o;
+        if (!other.canEqual((Object) this)) return false;
+        if (this.getUserId() != other.getUserId()) return false;
+        final Object this$slug = this.getSlug();
+        final Object other$slug = other.getSlug();
+        if (this$slug == null ? other$slug != null : !this$slug.equals(other$slug)) return false;
+        final Object this$title = this.getTitle();
+        final Object other$title = other.getTitle();
+        if (this$title == null ? other$title != null : !this$title.equals(other$title)) return false;
+        final Object this$body = this.getBody();
+        final Object other$body = other.getBody();
+        if (this$body == null ? other$body != null : !this$body.equals(other$body)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Post;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        result = result * PRIME + this.getUserId();
+        final Object $slug = this.getSlug();
+        result = result * PRIME + ($slug == null ? 43 : $slug.hashCode());
+        final Object $title = this.getTitle();
+        result = result * PRIME + ($title == null ? 43 : $title.hashCode());
+        final Object $body = this.getBody();
+        result = result * PRIME + ($body == null ? 43 : $body.hashCode());
+        return result;
+    }
+}
