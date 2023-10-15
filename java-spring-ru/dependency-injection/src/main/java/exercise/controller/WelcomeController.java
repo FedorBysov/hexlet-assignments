12501eddc81f57bd0cdf69add6ccac8ada7d47ec
FedorByssov <bysov1987@mail.ru>
2023-10-15 15:43:37 +0300

    HomeWork

--- java-spring-ru/dependency-injection/src/main/java/exercise/controller/WelcomeController.java
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/WelcomeController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@
 public class WelcomeController {
 
     @GetMapping(path = "")
